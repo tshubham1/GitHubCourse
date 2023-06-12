4351e7603691fcbcbb594b465ef5a06a0f31a1be
tshubham1 <tshubham102@gmail.com>
2023-06-12 18:11:37 +0530

    First Commit

---
+++ src/tests/HomePageTest.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class HomePageTest {
+	
+	public void test1() {
+		System.out.println("test1 -- HP");
+	}
+
+}
---
+++ src/tests/LoginPageTest.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class LoginPageTest {
+	
+	public void test1() {
+		System.out.println("test1 -- LP");
+	}
+}
